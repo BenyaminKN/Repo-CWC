019e4e543e3bccdf969903c2422a1f428996f46a
Benyamin <benykariminia@gmail.com>
2024-07-07 15:19:59 +0330

    Initial Commit

---
+++ RandomApp/RandomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAppApp.swift
+//  RandomApp
+//
+//  Created by Beny on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
